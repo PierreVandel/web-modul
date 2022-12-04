ef496e9a7ec6f73be9848f42bb66d3b511c6559e
PierreVandel <pierre.vdl@orange.fr>
2022-12-04 14:39:12 +0100

    Add consoleLog function

--- src/App.svelte
+++ src/App.svelte
@@ -10,28 +10,32 @@
     alert(`submitted ${_data}`);
   }
 
-    async function readTag() 
-    {
-        if ("NDEFReader" in window) {
-            const ndef = new NDEFReader();
-            try {
-            await ndef.scan();
-            ndef.onreading = event => {
-                const decoder = new TextDecoder();
-                for (const record of event.message.records) {
-                consoleLog("Record type:  " + record.recordType);
-                consoleLog("MIME type:    " + record.mediaType);
-                consoleLog("=== data ===\n" + decoder.decode(record.data));
-                _data = decoder.decode(record.data);
-                }
+  async function readTag() {
+    if ("NDEFReader" in window) {
+        const ndef = new NDEFReader();
+        try {
+        await ndef.scan();
+        ndef.onreading = event => {
+            const decoder = new TextDecoder();
+            for (const record of event.message.records) {
+            consoleLog("Record type:  " + record.recordType);
+            consoleLog("MIME type:    " + record.mediaType);
+            consoleLog("=== data ===\n" + decoder.decode(record.data));
+            _data = decoder.decode(record.data);
             }
-            } catch(error) {
-                consoleLog(error);
-            }
-        } 
-        else {
-            consoleLog("Web NFC is not supported.");
         }
+        } catch(error) {
+            consoleLog(error);
+        }
+    } 
+    else {
+        consoleLog("Web NFC is not supported.");
+    }
+  }
+
+  function consoleLog(data) {
+    var logElement = document.getElementById('log');
+    logElement.innerHTML += data + '\n';
     }
 
 </script>
