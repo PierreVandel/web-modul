827c55bd43a2321dfc95deed3b40e71826d2bf30
PierreVandel <pierre.vdl@orange.fr>
2022-12-04 10:00:05 +0100

    Add alert

--- src/App.svelte
+++ src/App.svelte
@@ -45,8 +45,9 @@
   <script>_data = _data</script>
   -->
   <button on:click={readTag}>Test NFC Read</button>
+  <h1>resultat : {_data}</h1>
   {#if _data}
-    <h1>resultat : {_data}</h1>
+    <script>handleSubmit()</script>
   {/if}
   
 </main>
